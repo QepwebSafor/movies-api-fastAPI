dff5cec5f7bdafd55b83d9884fdfebe3fa4ad56e
QepWeb <99823781+QepwebSafor@users.noreply.github.com>
2024-08-06 20:07:35 +0200

    Update database.py

--- config/database.py
+++ config/database.py
@@ -7,7 +7,6 @@ sqlite_file_name = "../database.sqlite"
 base_dir = os.path.dirname(os.path.realpath(__file__))
 
 database_url = f"sqlite:///{os.path.join(base_dir, sqlite_file_name)}"
-# Cambiar los permisos del archivo de la base de datos
 os.chmod(os.path.join(base_dir, sqlite_file_name), 0o777)
 engine = create_engine(database_url, echo=True)
 
